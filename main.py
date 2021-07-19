53c08e87989852cb8712f65d1c9bf96210fad17a
progs2002 <39672908+progs2002@users.noreply.github.com>
2021-07-19 15:00:19 +0530

    started working with sdl

--- main.py
+++
@@ -1,4 +0,0 @@
-from vector import *
-v1 = Vector(5,4)
-v2 = Vector(2,8)
-print(dotProduct(v1,v2))
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include "SDL.h"
+#include <iostream>
+
+int main(int argv, char** args)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_Window *window = SDL_CreateWindow("Hello SDL", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, 0);
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
+
+	bool isRunning = true;
+	SDL_Event event;
+
+	while (isRunning)
+	{
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				isRunning = false;
+				break;
+
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_ESCAPE)
+				{
+					isRunning = false;
+				}
+			}
+		}
+
+		SDL_RenderClear(renderer);
+		SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+
+		SDL_RenderPresent(renderer);
+	}
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+
+	return 0;
+}
